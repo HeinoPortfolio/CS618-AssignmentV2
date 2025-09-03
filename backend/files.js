9c221869e125bfd2be802c6a4698b888c6fe097f
Matthew Heino <61652462+HeinoPortfolio@users.noreply.github.com>
2025-09-03 15:26:14 +0000

    chore: review node server files

---
+++ backend/files.js
@@ -0,0 +1,13 @@
+// Writes to a file in the backend folder
+
+import { writeFileSync, readFileSync } from 'node:fs'
+
+const users = [{ name: 'Adam Ondra', email: 'adam.ondra@climb.ing' }]
+const usersJson = JSON.stringify(users)
+
+writeFileSync('backend/users.json', usersJson)
+
+const readUsersJson = readFileSync('backend/users.json')
+const readUsers = JSON.parse(readUsersJson)
+
+console.log(readUsers)
